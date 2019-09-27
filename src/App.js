9881098c757c866442eaec7251c8db14b56cbacb
cjkumaresh <test@test.com>
2019-09-27 09:54:20 +0530

    added name - setup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { hot } from "react-hot-loader";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
       <div className="App">
-        <h1> JSON resume </h1>
+        <h1> {this.props.name} </h1>
       </div>
     );
   }
